7217c03ded65714eaba0a7af86d65436a8b47b09
Katia Villavicencio <125634854+KatiaVillavicencio@users.noreply.github.com>
2023-04-27 18:24:48 -0500

    para presentar

--- miarchivo.js
+++ miarchivo.js
@@ -1,46 +1,48 @@
-
-
 function calcularIMC(peso, estatura) {
     return parseInt(peso / (estatura * estatura));
-}
-
-let saludo = false;
-while (saludo === false) {
+  }
+ 
+  let saludo = false;
+  while (saludo === false) {
     console.log("¡Hola!");
     saludo = confirm("Bienvenido, acepte para empezar");
     if (saludo === false) {
-        break; // Si el usuario da clic en "Cancelar", se detiene el ciclo
+      break; 
     }
     continue;
-}
-
-const nombre = prompt("Ingrese su nombre");
-let peso = parseFloat(prompt("Ingrese su peso en Kilos"));
-let estatura = parseFloat(prompt("Ingrese su estatura en Metros"));
-let sexo = prompt("Ingrese su sexo: f o m");
-
-let resultadoIMC = calcularIMC(peso, estatura);
-
-function imprimirResultadoIMC(nombre, resultadoIMC) {
+  }
+  
+  const nombre = prompt("Ingrese su nombre");
+  let peso = parseFloat(prompt("Ingrese su peso en Kilos"));
+  let estatura = parseFloat(prompt("Ingrese su estatura en Metros"));
+  let sexo = prompt("Ingrese su sexo: f o m");
+  
+  let resultadoIMC = calcularIMC(peso, estatura);
+  
+  function imprimirResultadoIMC(nombre, resultadoIMC) {
     if (resultadoIMC < 20) {
-        console.log(nombre + "," + " Tu índice de masa corporal es " + resultadoIMC + " = peso por debajo del normal");
-    }
-    else if (resultadoIMC >= 20 && resultadoIMC < 24) {
-        console.log(nombre + "," + " Tu índice de masa corporal es " + resultadoIMC + " = peso normal");
-        
+      console.log(nombre + "," + " Tu índice de masa corporal es " + resultadoIMC + " = peso por debajo del normal");
+    } else if (resultadoIMC >= 20 && resultadoIMC < 24) {
+      console.log(nombre + "," + " Tu índice de masa corporal es " + resultadoIMC + " = peso normal");
     } else if (resultadoIMC >= 24 && resultadoIMC < 29) {
-        console.log(nombre + "," + " Tu índice de masa corporal es " + resultadoIMC + " = peso superior al normal");
+      console.log(nombre + "," + " Tu índice de masa corporal es " + resultadoIMC + " = peso superior al normal");
     }
     if (resultadoIMC > 29) {
-        alert(nombre + "," + " Tu índice de masa corporal es " + resultadoIMC + " = Alerta de obesidad");
+      alert(nombre + "," + " Tu índice de masa corporal es " + resultadoIMC + " = Alerta de obesidad");
     }
-}
+  }
+  
+  if (sexo === "f") {
+    imprimirResultadoIMC(nombre, resultadoIMC);
+  } else if (sexo === "m") {
+    imprimirResultadoIMC(nombre, resultadoIMC);
+  }
+  
 
-if (sexo === "f") {
-    imprimirResultadoIMC(nombre, resultadoIMC);
-} else if (sexo === "m") {
-    imprimirResultadoIMC(nombre, resultadoIMC);
-}
+
+
+
+
 
 
 
